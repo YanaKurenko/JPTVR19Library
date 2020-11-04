e92d6251f562308860c10090ce1f4191140b9be0
pupil <pupil@ivkhk.ee>
2020-11-04 15:20:06 +0200

    git commit -m

--- src/entity/Reader.java
+++ src/entity/Reader.java
@@ -3,15 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package entity;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-/**
- *
- * @author Melnikov
- */
+
+ @Entity 
 public class Reader implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String firstname;
     private String lastname;
     private String phone;
@@ -36,7 +42,7 @@ public class Reader implements Serializable{
     public String getLastname() {
         return lastname;
     }
-
+    
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
@@ -56,6 +62,14 @@ public class Reader implements Serializable{
                 + ", lastname=" + lastname 
                 + ", phone=" + phone 
                 + '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
     
     
--- src/tools/savers/SaverToFile.java
+++ src/tools/savers/SaverToFile.java
@@ -14,6 +14,10 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -21,35 +25,48 @@ import java.util.List;
  */
 public class SaverToFile {
     public void save(List arrayList,String fileName) {
-                FileOutputStream fos = null;
-        ObjectOutputStream oos = null;
-        try {
-            fos = new FileOutputStream("readers");
-            oos = new ObjectOutputStream(fos);
-            oos.writeObject(arrayList);
-            oos.flush();
-        } catch (FileNotFoundException ex) {
-            System.out.println("Не найден файл");
-        } catch (IOException ex) {
-            System.out.println("Ошибка ввода/вывода");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPTVR19LibraryDev25PU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        for (int i = 0; i < arrayList.size(); i++) {
+            if("Reader".equals(arrayList.get(i).getClass().toString())){
+                List<Reader> listReader = (List<Reader>) arrayList;
+                if(listReader.get(i).getId()==null){
+                 em.persist(listReader.get(i));
+                }
+            }
         }
+        tx.commit();
+//                FileOutputStream fos = null;
+//        ObjectOutputStream oos = null;
+//        try {
+//            fos = new FileOutputStream("readers");
+//            oos = new ObjectOutputStream(fos);
+//            oos.writeObject(arrayList);
+//            oos.flush();
+//        } catch (FileNotFoundException ex) {
+//            System.out.println("Не найден файл");
+//        } catch (IOException ex) {
+//            System.out.println("Ошибка ввода/вывода");
+//        }
     }
     public List load(String fileName) {
-        List arrayList = new ArrayList();
-        List<Reader> listReaders = new ArrayList<>();
-        FileInputStream fis = null;
-        ObjectInputStream ois = null;
-        try {
-            fis = new FileInputStream(fileName);
-            ois = new ObjectInputStream(fis);
-            arrayList = (List) ois.readObject();
-        } catch (FileNotFoundException ex) {
-            System.out.println("Не найден файл");
-        } catch (IOException ex) {
-            System.out.println("Ошибка ввода/вывода");
-        } catch (ClassNotFoundException ex) {
-            System.out.println("Не найден класс");
-        }
-        return arrayList;
+//        List arrayList = new ArrayList();
+//        List<Reader> listReaders = new ArrayList<>();
+//        FileInputStream fis = null;
+//        ObjectInputStream ois = null;
+//        try {
+//            fis = new FileInputStream(fileName);
+//            ois = new ObjectInputStream(fis);
+//            arrayList = (List) ois.readObject();
+//        } catch (FileNotFoundException ex) {
+//            System.out.println("Не найден файл");
+//        } catch (IOException ex) {
+//            System.out.println("Ошибка ввода/вывода");
+//        } catch (ClassNotFoundException ex) {
+//            System.out.println("Не найден класс");
+//        }
+//        return arrayList;
     }
 }
